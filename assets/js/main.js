8aa5d79b18f562ab33ad22c5d7a82e9393653fe2
Faridhfatush <faridhfatush2024@gmail.com>
2022-06-26 00:52:57 +0300

    commit message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-// *** Write your code here***
+const clear_btn = document.querySelector('.btn-clear');
+let result=document.getElementById("inputText");
+
+function appendNumber(number){
+    document.getElementById("inputText").value+=number;
+}
+
+let compute=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert ("enter valid input");
+    }
+}
+clear_btn.addEventListener('click',()=>{
+    result.value='';
+})
+
+
+function delete_btn(){
+    result.value=result.value.slice(0,-1);
+}
+
+
+
